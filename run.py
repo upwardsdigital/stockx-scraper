6933b853a434553aeb73cc36f0a5a60d1e982013
Kaiduev <kaiduevbaktygul07@gmail.com>
2023-09-16 01:52:49 +0600

    Add some logic

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 import asyncio
+import time
+
 import requests
 import json
 from pathlib import Path
@@ -18,11 +20,12 @@ NUM_PROCESSES = int(config('NUM_PROCESSES'))
 
 def run_async_scrape_slugs(category):
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(stockx.scrape_slugs(category))
+    return loop.run_until_complete(stockx.scrape_slugs(category))
 
 
 def run_async_scrape_product(url):
     loop = asyncio.get_event_loop()
+    time.sleep(0.5)
     return loop.run_until_complete(stockx.scrape_product(url))
 
 
@@ -32,31 +35,41 @@ async def run():
     # categories = np.array(stockx.get_all_categories())
     # split_categories = np.array_split(categories, 4)
     # categories_by_server_number = split_categories[SERVER_NUMBER - 1]
-    # categories_by_server_number = ["https://stockx.com/" + item for item in categories_by_server_number]
-    # pool = multiprocessing.Pool(processes=NUM_PROCESSES)
-    # pool.map(run_async_scrape_slugs, categories_by_server_number)
-    # pool.close()
-    # pool.join()
-    # print("All processes have completed successfully")
+    # categories = stockx.get_all_categories()
+    # categories = ["https://stockx.com/" + item for item in categories]
+    # formatted_categories_by_process_number = np.array_split(
+    #     categories, len(categories) / 15
+    # )
+    # for c in formatted_categories_by_process_number:
+    #     pool = multiprocessing.Pool(processes=NUM_PROCESSES)
+    #     slugs = pool.map(run_async_scrape_slugs, c.tolist())
+    #     slugs = [item for sublist in slugs for item in sublist]
+    #     pool.close()
+    #     pool.join()
+    #     with open('results/slugs_new.json', 'r') as f1:
+    #         s = json.load(f1)
+    #     new_slugs = s + slugs
+    #     with open('results/slugs_new.json', 'w') as f2:
+    #         json.dump(new_slugs, f2)
+    #     print("All processes have completed successfully")
 
-    with open('results/slugs.json', 'r') as f1:
+    with open('results/slugs_new.json', 'r') as f1:
         slugs = np.array(json.load(f1))
-    split_slugs = np.array_split(slugs, 4)
-    slugs_by_server_number = split_slugs[SERVER_NUMBER - 1]
-    formatted_slugs_by_server_number = [
+    slugs = [dict(s) for s in set(frozenset(d.items()) for d in slugs)][20050:]
+    slugs = [
         "https://stockx.com/" + slug_by_server_number.get('slug')
-        for slug_by_server_number in slugs_by_server_number
+        for slug_by_server_number in slugs
     ]
-    formatted_slugs_by_server_number_splitted = np.array_split(
-        formatted_slugs_by_server_number, len(formatted_slugs_by_server_number) / 4
+    formatted_slugs = np.array_split(
+        slugs, len(slugs) / 10
     )
-    for slug_parts in formatted_slugs_by_server_number_splitted:
+    for slug_parts in formatted_slugs:
         pool = multiprocessing.Pool(processes=NUM_PROCESSES)
         products = pool.map(run_async_scrape_product, slug_parts.tolist())
-        products_formatted = formatted_products(products)
-        post_products(products_formatted)
         pool.close()
         pool.join()
+        products_formatted = formatted_products(products)
+        post_products(products_formatted)
         print("All processes have completed successfully")
 
 
